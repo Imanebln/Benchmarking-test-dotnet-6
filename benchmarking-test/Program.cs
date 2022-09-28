fbdeae77a8e395081f5e88cd601f74aa94583a2f
Imanebln <boulouane.imane@gmail.com>
2022-09-28 18:16:29 +0100

    added Benchmarking LINQ Performance

---
+++ benchmarking-test/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace benchmarking_test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BenchmarkLINQPerformance>();
+
+        }
+    }
+}
